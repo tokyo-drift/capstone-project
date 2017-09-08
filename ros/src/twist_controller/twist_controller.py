89baf704f5e2c0f70c026c4a997f9de74933d97a
ddigges <deborahgertrude_digges@intuit.com>
2017-09-08 23:35:24 +0530

    Implement constructor

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,3 +1,4 @@
+from pid import PID
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
@@ -5,10 +6,22 @@ ONE_MPH = 0.44704
 
 class Controller(object):
     def __init__(self, *args, **kwargs):
-        # TODO: Implement
-        pass
+        
+        vehicle_mass = kwargs['vehicle_mass']
+        fuel_capacity = kwargs['fuel_capacity']
+        brake_deadband = kwargs['brake_deadband']
+        decel_limit = kwargs['decel_limit']
+        accel_limit = kwargs['accel_limit']
+        wheel_radius = kwargs['wheel_radius']
+        wheel_base = kwargs['wheel_base']
+        steer_ratio = kwargs['steer_ratio']
+        max_lat_accel = kwargs['max_lat_accel']
+        max_steer_angle = kwargs['max_steer_angle']
+
+        self.linear_pid = PID(0.12, 0.0005, 0.04, -accel_limit, accel_limit)
+    
 
     def control(self, proposed_linear_velocity, proposed_angular_velocity, current_linear_velocity):
-        # TODO: Change the arg, kwarg list to suit your needs
-        # Return throttle, brake, steer
+        
+
         return 1., 0., 0.
