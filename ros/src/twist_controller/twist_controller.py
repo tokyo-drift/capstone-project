23ffa441428ccb912c8d670a27bd20f336112b54
ddigges <deborahgertrude_digges@intuit.com>
2017-09-10 22:43:57 +0530

    DBW: Curtail throttle between 0 and 1

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,13 +1,13 @@
 from pid import PID
 from yaw_controller import YawController
-
+import math
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
 
 class Controller(object):
     def __init__(self, *args, **kwargs):
-        
+
         vehicle_mass = kwargs['vehicle_mass']
         fuel_capacity = kwargs['fuel_capacity']
         brake_deadband = kwargs['brake_deadband']
@@ -20,8 +20,10 @@ class Controller(object):
         max_steer_angle = kwargs['max_steer_angle']
         min_speed = 0 # TODO: What is a good value for min speed?
 
-        self.linear_pid = PID(0.12, 0.0005, 0.04, -accel_limit, accel_limit)
+
+        self.linear_pid = PID(kp=0.2, ki=0.005, kd=0.1, mn=decel_limit, mx=0.5 * accel_limit)
         self.yaw_controller = YawController(wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle)
+
 
     def control(self, proposed_linear_velocity, proposed_angular_velocity, current_linear_velocity):
         linear_velocity_error = proposed_linear_velocity - current_linear_velocity
@@ -35,9 +37,9 @@ class Controller(object):
         throttle = velocity_correction
 
         if(throttle < 0):
-            brake = 1000 * abs(throttle)
-            throttle = 0.
+        #    brake = 1000 * abs(throttle)
+            throttle = 0
 
         steering = self.yaw_controller.get_steering(proposed_linear_velocity, proposed_angular_velocity, current_linear_velocity)
-
+        
         return throttle, brake, steering
