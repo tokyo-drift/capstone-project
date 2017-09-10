23110ee669fb4a318112638135246fd46a386e91
ddigges <deborahgertrude_digges@intuit.com>
2017-09-10 15:43:24 +0530

    Performance improvement

--- ros/src/styx/server.py
+++ ros/src/styx/server.py
@@ -12,7 +12,12 @@ from conf import conf
 sio = socketio.Server()
 app = Flask(__name__)
 bridge = Bridge(conf)
-msgs = []
+#
+# Changed to only send the latest message for each topic, rather
+# than queuing out of date messages. Based on
+# https://github.com/amakurin/CarND-Capstone/commit/9809bc60d51c06174f8c8bfe6c40c88ec1c39d50
+#
+msgs = {}
 
 dbw_enable = False
 
@@ -22,7 +27,7 @@ def connect(sid, environ):
 
 def send(topic, data):
     s = 1
-    msgs.append((topic, data))
+    msgs[topic] = data
     #sio.emit(topic, data=json.dumps(data), skip_sid=True)
 
 bridge.register_server(send)
@@ -35,7 +40,7 @@ def telemetry(sid, data):
         bridge.publish_dbw_status(dbw_enable)
     bridge.publish_odometry(data)
     for i in range(len(msgs)):
-        topic, data = msgs.pop(0)
+        topic, data = msgs.popitem()
         sio.emit(topic, data=data, skip_sid=True)
 
 @sio.on('control')
