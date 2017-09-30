79d32bf2853f0a82b857fdda63ee51b4b87fe547
ddigges <deborahgertrude_digges@intuit.com>
2017-09-30 20:28:03 +0530

    Compute torque from vehicle parameters rather than use hard coded value

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -8,23 +8,23 @@ ONE_MPH = 0.44704
 class Controller(object):
     def __init__(self, *args, **kwargs):
 
-        vehicle_mass = kwargs['vehicle_mass']
-        fuel_capacity = kwargs['fuel_capacity']
-        brake_deadband = kwargs['brake_deadband']
-        decel_limit = kwargs['decel_limit']
-        accel_limit = kwargs['accel_limit']
-        wheel_radius = kwargs['wheel_radius']
-        wheel_base = kwargs['wheel_base']
-        steer_ratio = kwargs['steer_ratio']
-        max_lat_accel = kwargs['max_lat_accel']
-        max_steer_angle = kwargs['max_steer_angle']
+        self.vehicle_mass = kwargs['vehicle_mass']
+        self.fuel_capacity = kwargs['fuel_capacity']
+        self.brake_deadband = kwargs['brake_deadband']
+        self.decel_limit = kwargs['decel_limit']
+        self.accel_limit = kwargs['accel_limit']
+        self.wheel_radius = kwargs['wheel_radius']
+        self.wheel_base = kwargs['wheel_base']
+        self.steer_ratio = kwargs['steer_ratio']
+        self.max_lat_accel = kwargs['max_lat_accel']
+        self.max_steer_angle = kwargs['max_steer_angle']
         min_speed = 0 # TODO: What is a good value for min speed?
 
 
         #self.linear_pid = PID(kp=0.2, ki=0.005, kd=0.1, mn=decel_limit, mx=0.5 * accel_limit)
-        self.linear_pid = PID(kp=0.8, ki=0, kd=0.05, mn=decel_limit, mx=0.5 * accel_limit)
-        self.yaw_controller = YawController(wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle)
-        self.steering_pid = PID(kp=0.15, ki=0.001, kd=0.1, mn=-max_steer_angle, mx=max_steer_angle)
+        self.linear_pid = PID(kp=0.8, ki=0, kd=0.05, mn=self.decel_limit, mx=0.5 * self.accel_limit)
+        self.yaw_controller = YawController(self.wheel_base, self.steer_ratio, min_speed, self.max_lat_accel, self.max_steer_angle)
+        self.steering_pid = PID(kp=0.15, ki=0.001, kd=0.1, mn=-self.max_steer_angle, mx=self.max_steer_angle)
 
     def reset(self):
         self.linear_pid.reset()
@@ -39,7 +39,8 @@ class Controller(object):
         throttle = velocity_correction
 
         if(throttle < 0):
-            brake = 1000 * abs(throttle)
+            deceleration = abs(throttle)
+            brake = (self.vehicle_mass + self.fuel_capacity * GAS_DENSITY) * self.wheel_radius * deceleration if deceleration > self.brake_deadband else 0.
             throttle = 0
 
         predictive_steering = self.yaw_controller.get_steering(proposed_linear_velocity, proposed_angular_velocity, current_linear_velocity)
